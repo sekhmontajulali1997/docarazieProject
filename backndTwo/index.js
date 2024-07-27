ecf8e3687dd2e3b1079c46f17c235179e17ad90f
skMontajulAli <sekhmontajulali1997@gmail.com>
2024-07-27 09:58:20 +0530

    docaraization

---
+++ backndTwo/index.js
@@ -0,0 +1,28 @@
+import express from "express";
+import colors from "colors";
+import dotenv from "dotenv";
+
+// environment varible
+
+dotenv.config();
+const PORT = process.env.PORT || 5755;
+// init express
+const app = express();
+
+// create route
+
+app.get("/", (req, res) => {
+  res.status(200).json(
+    { messgage: "this is backndTwo good" }
+    //   [
+    //   { name: "mon", age: 20 },
+    //   { name: "rima", age: 20 },
+    //   { name: "eliza", age: 20 },
+    //   { name: "parul", age: 20 },
+    // ]
+  );
+});
+//listen server
+app.listen(PORT, () => {
+  console.log(`SERVER IS RUNIN ON ${PORT}`.bgGreen.black);
+});
